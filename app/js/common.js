c89c09fd22c34f2898d1964815d952858b19bf2d
Pauk Roman <pauk83258325@gmail.com>
2017-08-07 22:32:42 +0300

    footer

--- app/js/common.js
+++ app/js/common.js
@@ -1,7 +1,16 @@
 $(function() {
 
 	// Custom JS
-	$('button').click(function(){});
+	$('.burger').click(function(){
+		var th = $(this);
+		$('.toggleMenu').slideToggle('slow', function() {
+			th.toggleClass('burger-anim');
+		});
+	});
+
+	$('.buttonDown').click(function(){
+		$('html, body').animate({scrollTop: $('#whatWeDo').offset().top}, 'slow');
+	});
 
 	$('.mainCarousel').owlCarousel({
 		loop:true,
@@ -20,4 +29,23 @@ $(function() {
     navText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
 	});
 
+
+	$('.skillItem').each(function() {
+		var itNum = $(this).find('.number').attr('data-number') * 3.6;
+		$(this).find('.skillItem__circle').append($('<div class="circle-rot"></div>'));
+		$(this).find('.circle-rot').css('transform', 'rotate(-225deg) rotate('+ itNum +'deg)');
+	});
+
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > $(this).width()){
+			$('.buttonTop').addClass('buttonTop-active');
+		} else {
+			$('.buttonTop').removeClass('buttonTop-active');
+		}
+	});
+
+	$('.buttonTop').click(function() {
+		$('html, body').animate({scrollTop: 0}, 'slow', 'swing');
+	});
+	
 });
